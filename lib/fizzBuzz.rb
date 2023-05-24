e87403a658cf07b52aceceb15c162d02da7bbafa
JessJiggs <verheuljess@gmail.com>
2023-05-24 21:19:03 +0200

    create range works

--- lib/fizzBuzz.rb
+++ lib/fizzBuzz.rb
@@ -1,30 +1,41 @@
-class FizzBuzz
-n =
-  def create_range(n)
-    range = [1..n]
-    return range
-  end
+# class FizzBuzz
 
-  def fizzerBuzzer(range)
-    range.each {|x| }
-    if x % 3 && x % 5 == 0
-      # replace (is that a thing?) x with FizzBuzz
-    elsif x % 3 == 0
-      # replace x with Fizz
-    elsif x % 5 == 0
-      # replace x with Buzz
-    # else x % 3,5 !=0
-    #   do nothing
-    end
+  n = 30
+  a = 1
+  b = n
+  range = (Range.new(a, b, false)).to_a
+  puts range
+ 
 
-    end
 
-if
+  # def fizzerBuzzer(range)
+  #   # array.map { |o| if condition(o) then obj else o }
+  #   range.each_with_index do {|element,index|  }
+  #     if x % 3 == 0 && x % 5 == 0
+  #       # replace (is that a thing?) x with FizzBuzz
+  #     elsif x % 3 == 0
+  #       # replace x with Fizz
+  #     elsif x % 5 == 0
+  #       # replace x with Buzz
+  #     # else x % 3,5 !=0
+  #     #   do nothing
+  #     end
+  #   end
+  #   return string_array
+  # end
 
-else
 
-end
+#   string_array.collect! do |element|
+#     case element
+#     when x % 3 == 0 && x % 5 == 0
+#       'FizzBuzz'
+#     when x % 3 == 0
+#       'Fizz'
+#     when x % 5 == 0
+#       'Buzz'
+#     else
+#       element
+#     end
+#   end
 
-  end
-
-end
+# end
