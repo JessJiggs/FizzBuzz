ec667755f929414fee7154b79c30e6ba5d053f7c
JessJiggs <verheuljess@gmail.com>
2023-06-06 12:42:26 +0200

    in a method

--- lib/fizzBuzz.rb
+++ lib/fizzBuzz.rb
@@ -1,15 +1,10 @@
 
-puts "Please enter a number between 1-10000"
+puts "Please enter a number between 1 - 10 000"
 n = gets.chomp.to_i
 
-n = 45
-a = 1
-b = n
-range = (Range.new(a, b, false)).to_a
-
-string_array = []
-
-# def FizzerBuzzer(range)
+def FizzerBuzzer(n)
+  range = (Range.new(1, n, false)).to_a
+  string_array = []
   range.each do |i|
     if i % 3 == 0 && i % 5 == 0
       string_array << 'FizzBuzz'
@@ -18,57 +13,10 @@ string_array = []
     elsif i % 5 == 0
       string_array << 'Buzz'
     else
-      string_array << i
-    end
-  end
-#   return string_array
-# end
-
-puts string_array
-
-###################
-# OR
-string_array = []
-# array = []
-# def fizzerBuzzer(range)
-  # array = []
-  # string_array = array
-  range.each do |i|
-    if i % 3 == 0 && i % 5 == 0
-      string_array << "FizzBuzz"
-    elsif i % 3 == 0
-      string_array << "Fizz"
-    elsif i % 5 == 0
-      string_array << "Buzz"
-    else
       string_array << i.to_s
     end
-  # return array
   end
-# end
+  return string_array
+end
 
-# string_array = array
-puts string_array
-###################
-
-# can delete below if you cant solve it
-###################
-#   range.collect! do |element|
-#     case element
-#     when element % 3 == 0 && element % 5 == 0
-#       string_array << 'FizzBuzz'
-#     when element % 3 == 0
-#       string_array << 'Fizz'
-#     when element % 5 == 0
-#       string_array << 'Buzz'
-#     else
-#       string_array << element
-#     end
-#   end
-# puts string_array
-
-  # def fizzerBuzzer(range)
-
-  #  return string_array
-  # end
-###################
+puts FizzerBuzzer(n)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,24 +7,23 @@
 #     end
 #   end
 # end
-#############################
-# Hmmmm
-#############################
-# describe range do #this is not a method though - so how do ?
-#   describe "range" do # "range" or range
-#     it "creates a range from 1..n using the argument passed (n = integer)" do
-#       range = FizzBuzz.new
-#       n = 9
-#       expect(range(1,n)).to [1..9]
-#     end
-#   end
 
-#   describe "FizzerBuzzer" do
-#     it "checks if i % 3 == 0 && i % 5 == 0; push 'FizzBuzz' into string_array
-#         checks elseif i % 3 == 0; push 'Fizz' into string_array
-#         checks elseif i % 5 == 0; push 'Buzz' into string_array
-#         checks else i != 0; push i into string_array"
-#     string_array = []
-#     range = [1..15]
-#     expect(FizzerBuzzer(1,15)).to [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
-#   end
+#############################
+describe FizzerBuzzer do
+  describe range do
+    it "creates a range from 1..n using the argument passed (n = integer), including n" do
+      range = FizzBuzz.new
+      n = 9
+      expect(range(1,n)).to [1..9]
+    end
+  end
+
+  describe "FizzerBuzzer" do
+    it "checks if i % 3 == 0 && i % 5 == 0; push 'FizzBuzz' into string_array
+        checks elseif i % 3 == 0; push 'Fizz' into string_array
+        checks elseif i % 5 == 0; push 'Buzz' into string_array
+        checks else i != 0; push i into string_array"
+    string_array = []
+    range = [1..15]
+    expect(FizzerBuzzer(1,15)).to [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+  end
